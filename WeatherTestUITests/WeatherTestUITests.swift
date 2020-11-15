b286a113d36fc175500ee3d2597b6753ca10c804
Pattarapol Sawasdee <pattarapol.s@kbtg.tech>
2020-11-16 01:48:31 +0700

    add ui test

--- WeatherTestUITests/WeatherTestUITests.swift
+++ WeatherTestUITests/WeatherTestUITests.swift
@@ -8,35 +8,45 @@
 import XCTest
 
 class WeatherTestUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testMain() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText("bangkok")
+        app.firstMatch.tap()
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    func testMainInCaseError() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText("xxxxxx")
+        app.firstMatch.tap()
+        XCTAssertEqual("Error: status 404", app.staticTexts.element(boundBy: 1).label)
     }
+    
+    func testMainChangeTempType() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText("bangkok")
+        app.firstMatch.tap()
+        app.buttons.element(boundBy: 1).tap()
+    }
+    
+    func testForecast() throws {
+        let app = XCUIApplication()
+        app.launch()
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText("bangkok")
+        app.firstMatch.tap()
+        app.navigationBars.children(matching: .button).firstMatch.tap()
+        app.tables.firstMatch.swipeUp()
+    }
+    
 }
