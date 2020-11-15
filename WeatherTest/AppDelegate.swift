5821a024381df742d4e2681f83445398eee92dff
Pattarapol Sawasdee <pattarapol.s@kbtg.tech>
2020-11-16 01:43:35 +0700

    remove comment

--- WeatherTest/AppDelegate.swift
+++ WeatherTest/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  WeatherTest
-//
-//  Created by Pattarapol Sawasdee on 13/11/2563 BE.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- WeatherTest/SceneDelegate.swift
+++ WeatherTest/SceneDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  SceneDelegate.swift
-//  WeatherTest
-//
-//  Created by Pattarapol Sawasdee on 13/11/2563 BE.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
